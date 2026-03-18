030dd709807039a129138ea0f34c4f4369807e92
Makedonsky <conquerror@macedonian.com>
2026-03-18 08:12:55 +0500

    Рефакторинг: Слоистая архитектура

---
+++ app/api/router.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+from app.api.routers.tasks import router as tasks_router
+
+
+api_router = APIRouter()
+api_router.include_router(tasks_router)
---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from app.models.task import TaskORM
+
+__all__ = ["TaskORM"]
